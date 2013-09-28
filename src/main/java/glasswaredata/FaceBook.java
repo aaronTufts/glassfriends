285f6a9bbaf4b1fe96f5d5db87a5701f413bf5d3
Caroline Marcks <caroline.marcks@tufts.edu>
2013-09-28 13:49:47 -0400

    template facebook bean and service files

---
+++ src/main/java/glasswaredata/FaceBook.java
@@ -0,0 +1,16 @@
+package glasswaredata;
+
+public class FaceBook {
+	private String name;
+	public FaceBook(String name){
+		this.setName(name);
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
---
+++ src/main/java/glasswareservice/FaceBookService.java
@@ -0,0 +1,5 @@
+package glasswareservice;
+
+public class FaceBookService {
+
+}
